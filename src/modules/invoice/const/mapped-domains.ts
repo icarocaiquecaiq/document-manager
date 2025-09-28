7410c60a1ba9ba580a5f396c0eac71404740e1b6
icarocaiquecaiq <icarocaiquevp16@gmail.com>
2025-09-28 00:54:42 -0300

    Fix: http server error

--- src/modules/invoice/const/mapped-domains.ts
+++ src/modules/invoice/const/mapped-domains.ts
@@ -1,13 +1,13 @@
 export const mappedDomains = {
-    'faturas.mercadolivre.com.br': {
+    'www.mercadolivre.com.br': {
         parser: 'MercadoLivreParser',
     },
-    'www.amazon.com.br': {
-        parser: 'AmazonParser',
+    'www.asaas.com': {
+        parser: 'AsaasParser',
     },
 };
 
 export const localTestUrlMap = {
-    'www.asaas.com': 'http://127.0.0.1:8080/asaas-invoice.html',
-    'www.mercadolivre.com.br': 'http://127.0.0.1:8080/mercado-livre-invoice.html',
+    'www.mercadolivre.com.br': 'http://localhost:8080/mercado-livre-invoice.html',
+    'www.asaas.com': 'http://localhost:8080/asaas-invoice.html',
 };
--- src/modules/invoice/services/scraping.service.ts
+++ src/modules/invoice/services/scraping.service.ts
@@ -2,8 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import pdf from 'pdf-parse';
 import { PdfDocumentParser } from '../parses/pdf-document.parser';
 import {} from '../parses/asaas-parser';
-import { localTestUrlMap } from '../const/mapped-domains';
-import * as cheerio from 'cheerio';
+import { localTestUrlMap, mappedDomains } from '../const/mapped-domains';
 import apiClientHttp from 'src/utils/api';
 import { ParserFactory } from '../parses/parser-factory';
 
@@ -23,19 +22,19 @@ export class ScrapingService {
         let urlToFetch = targetUrl;
         const domain = new URL(targetUrl).hostname;
 
-        if (!Object.keys(localTestUrlMap).includes(domain)) {
+        if (!Object.keys(mappedDomains).includes(domain)) {
             throw new BadRequestException(`Domain '${domain}' is not supported.`);
         }
 
-        console.log(localTestUrlMap[domain]);
         if (process.env.ENVIROMENT === 'dev' && localTestUrlMap[domain]) {
-            console.log(`[TEST MODE] Rerouting ${targetUrl} to ${localTestUrlMap[domain]}`);
+            console.log(`[DEV MODE] Rerouting ${targetUrl} to ${localTestUrlMap[domain]}`);
             urlToFetch = localTestUrlMap[domain];
         }
 
         const parser = this.parserFactory.getParserForDomain(domain);
 
         console.log(`Fetching content from: ${urlToFetch}`);
+
         const response = await apiClientHttp.get(urlToFetch);
 
         return parser.parse(response.data);
